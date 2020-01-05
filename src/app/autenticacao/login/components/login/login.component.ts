1672b61f70bb51bca4d13f3754c91dbfbbd370cb
RodPSouza <rod.p.souza@hotmail.com>
2020-01-05 20:25:52 -0300

    C. Udemy - Form. Angular 8 - Integ. API Restfull - 17. Criando o serviço de login - parte 2/2

--- src/app/autenticacao/login/components/login/login.component.ts
+++ src/app/autenticacao/login/components/login/login.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { MatSnackBar } from '@angular/material';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {MatSnackBar} from '@angular/material';
 
-import { Login } from '../../models';
-import { LoginService} from '../../services';
+import {Login} from '../../models';
+import {LoginService} from '../../services';
 
 @Component({
-  selector: 'app-login',
+  selector: 'app-login-pf',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
@@ -34,11 +34,35 @@ export class LoginComponent implements OnInit {
 
   logar() {
     if (this.form.invalid) {
-      this.snackBar.open('Dados inválidos', 'Erro');
       return;
     }
     const login: Login = this.form.value;
-    alert('Email:' + login.email + ', senha: ' + login.senha);
+    this.loginService.logar(login) //
+      .subscribe(
+        data => {
+          console.log(JSON.stringify(data));
+          localStorage.token = data.data.token;
+          const usuarioData = JSON.parse(
+            atob(data.data.token.split('.')[1]));
+          console.log(JSON.stringify(usuarioData));
+          if (usuarioData.role === 'ROLE_ADMIN') {
+            alert('Deve redirecionar para a página de admin');
+            // this.router.navigate(['/admin']);
+          } else {
+            alert('Deve redirecionar para a página de funcionário');
+            // this.router.navigate(['/funcionario']);
+          }
+        },
+        err => {
+          console.log(JSON.stringify(err));
+          let msg = 'Tente novamente em instantes.';
+          if (err.status === 401) {
+            msg = 'Email/senha inválido(s).';
+          }
+          this.snackBar.open(msg, 'Erro');
+        }
+      );
+
 
   }
 
